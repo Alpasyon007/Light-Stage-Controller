dffd6ed2be5b25f8d5e42288b816ee49245f59d3
Alp Orgun <alpasyon07@gmail.com>
2022-11-09 15:22:28 +0000

    added submodules and changed some code

--- rpi-source/c++/src/listpi.cpp
+++ rpi-source/c++/src/listpi.cpp
@@ -1,37 +1,43 @@
+// pigpio
 #include <pigpio.h>
 #include <signal.h>
 
+// std
 #include <iostream>
 
+// Helpers
 #include "StepperMotorController.h"
 #include "Serial.h"
 
+#define MOTOR_STEP_PIN 16
+#define MOTOR_DIRECTION_PIN 21
+#define MOTOR_STEP_DELAY_PIN 0.003f
+
 int main() {
 	if (gpioInitialise() < 0) return -1;
-	Serial serial("/dev/ttyUSB0", 9600);
-	// std::cout << "Initalised GPIO" << std::endl;
 
-	// StepperMotorController stepperMotor(16, 21, 0.003f);
-	// stepperMotor.StepForward(200); // 360 Degrees
-
-	// time_sleep(1);
-
-	// for(int i = 0; i < 10; i++) {
-	// 	stepperMotor.StepForward(20);
-	// 	time_sleep(1);
-	// }
-	
-	// gpioWrite(21, 0);
+	Serial serial("/dev/ttyAMA0", 9600);
+	std::cout << "Initalised GPIO" << std::endl;
+	StepperMotorController stepperMotor(MOTOR_STEP_PIN, MOTOR_DIRECTION_PIN, MOTOR_STEP_DELAY_PIN);
 
 	while(true) {
-		serial.Write(0);
+		stepperMotor.StepForward(200); // 360 Degrees
 		time_sleep(1);
 
-		for(int i = 0; i < 144; i++) {
-			serial.Write(i);
+		for(int i = 0; i < 10; i++) {
+			stepperMotor.StepForward(20);
 			time_sleep(1);
+
+			serial.Write(0);
+			time_sleep(1);
+
+			for(int i = 0; i < 144; i++) {
+				serial.Write(i);
+				time_sleep(1);
+			}
 		}
 	}
 
+	gpioWrite(21, 0);
 	gpioTerminate();
 }
